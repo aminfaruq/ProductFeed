3b561ef0e88a5235c6bc8f1cb0a25a01772dce04
Amin Faruq <aminfaruk.fa@gmail.com>
2024-04-20 00:18:34 +0800

    update CI

--- ProductFeedAPIEndToEndTests/ProductFeedAPIEndToEndTests.swift
+++ ProductFeedAPIEndToEndTests/ProductFeedAPIEndToEndTests.swift
@@ -44,7 +44,7 @@ final class ProductFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         
         return receivedResult
     }
